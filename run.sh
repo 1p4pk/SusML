fe8a3c428944404728f76e49385a5549be760af5
Ramin Gharib <ramin.gharib@student.hpi.uni-potsdam.de>
2020-06-06 12:57:31 +0200

    change run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mpirun -n 2 python src/malware_prediction/main_processing.py
-mpirun -n 2 python src/malware_prediction/main_model.py
+mpirun -n 2 python3 src/malware_prediction/main_processing.py 20000
+# mpirun -n 2 python src/malware_prediction/main_model.py
