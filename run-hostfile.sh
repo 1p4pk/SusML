5ff548fc6e9f6ec92d362d5967ae0905936b1ba2
Ramin Gharib <ramin.gharib@student.hpi.uni-potsdam.de>
2020-06-25 17:03:31 +0200

    update run-hostfile.sh file

--- run-hostfile.sh
+++ run-hostfile.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# $1=training row number
+# $2=test row number
+# $3=optimizer name
+# $4=loss function
 mpirun -n 2 --hostfile hostfile python3 src/malware_prediction/main_processing.py $1 $2
-mpirun -n 2 --hostfile hostfile python3 src/malware_prediction/main_model.py $1
+mpirun -n 2 --hostfile hostfile python3 src/malware_prediction/main_model.py $1 $3 $4
 mpirun -n 2 --hostfile hostfile python3 src/malware_prediction/main_eval.py $2
